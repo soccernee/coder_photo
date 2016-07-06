f59b4084ab11a21028c7554b1cc0abe06dcd9cac
Harley Trung <harley@harley.io>
2016-07-06 15:52:34 +0700

    milestone 4

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'rails-assets-tether'
 gem 'rails_12factor'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+# Adapted from: https://leonid.shevtsov.me/post/need-random-sample-images-why-not-imgur/
+gallery = JSON.parse(open('http://imgur.com/gallery.json').read)['data']
+
+# construct URLs
+gallery.each {|i| i['url'] = "http://imgur.com/#{i['hash']}#{i['ext']}" }
+# select images that aren't too big
+gallery.select {|i| i['size'] < 200_000 }
+# or images that aren't too small
+gallery.select {|i| i['width'] > 400 && i['height'] > 400 }
+# select only PNG images
+gallery.select {|i| i['ext'] == '.png'}
+
+gallery.each do |image|
+  Photo.create username: Faker::Internet.user_name,
+    caption: Faker::Lorem.sentence, likes_count: Faker::Number.number(4),
+    url: image['url'], created_at: Faker::Time.between(1.year.ago, Time.now)
+end
+
+# update all photos with missing images
+Photo.where(url: nil).each do |photo|
+  photo.update_column :url, "https://unsplash.it/500"
+end
